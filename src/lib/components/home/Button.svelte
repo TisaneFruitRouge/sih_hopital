f4030f9545a2efc8b12c580e182e2fd08d3bd4c7
Vincent WENDLING <vincent.wendling@lilo.org>
2023-10-18 16:22:31 +0200

    Initial commit

---
+++ src/lib/components/home/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let onClick: () => void;
+    export let label: string;
+</script>
+
+<button 
+    class="border-solid border-black border hover:bg-gray-100 duration-200 p-2" 
+    on:click={onClick}
+>
+    {label}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Button from "$lib/components/home/Button.svelte";
+    let count = 0;
+</script>
+
+
+<div class="w-screen h-screen justify-center items-center flex flex-col gap-4">
+    <p>Compteur: <b class="text-red-500">{count}</b></p>
+    <div class="flex gap-2">
+        <Button onClick={() => count++} label="Ajouter 1"/>
+        <Button onClick={() => count = 0} label="Reset" />
+    </div>
+    
+</div>
