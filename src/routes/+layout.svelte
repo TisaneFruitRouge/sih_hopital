9eea9e0a01de53363ae000150b3b7ca5d9725a5a
Vincent WENDLING <vincent.wendling@lilo.org>
2023-11-16 10:27:04 +0100

    added shit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,5 @@
     import '../app.css';
 </script>
 
-
+<img src="https://picsum.photos/200" class="w-screen h-12" alt="Logo de l'hôpital"/>
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,63 @@
 <script lang="ts">
-    import Button from "$lib/components/home/Button.svelte";
-    let count = 0;
+    import type { LayoutData } from "./$types";
+
+    export let data: LayoutData;
+
+    let medicalActs = data.medicalActs;
+    let users = data.users;
+    console.log(medicalActs)
+    let filterValue: string = "";
+
+    const filterByPatients = (filter: string) => {
+        if (filter === "") {
+            return medicalActs;
+        } else {
+            return medicalActs.filter(medicalAct => {
+                let user = users.filter(user => user.id === medicalAct.user_id)[0];
+
+                return `${user.first_name} ${user.name}`.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
+            });
+        }
+    }
+
+    $: filteredMedicalActs = filterByPatients(filterValue);
+
+    const getTime = (date: Date) => {
+        let h = date.getHours();
+        let m = date.getMinutes();
+
+        return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}`
+    }
 </script>
 
 
-<div class="w-screen h-screen justify-center items-center flex flex-col gap-4">
-    <p>Compteur: <b class="text-red-500">{count}</b></p>
-    <div class="flex gap-2">
-        <Button onClick={() => count++} label="Ajouter 1"/>
-        <Button onClick={() => count = 0} label="Reset" />
-    </div>
-    
+<div class="w-screen h-screen flex flex-col gap-4 p-4 items-center">
+    <input
+        class="input input-bordered w-full max-w-md" 
+        type="text" 
+        bind:value={filterValue} 
+        placeholder="Nom du patient"
+    >
+    <table class="table table-zebra">
+        <thead>
+            <tr>
+              <th>Date</th>
+              <th>Heure</th>
+              <th>Examen</th>
+              <th>Patient</th>
+              <th>Confirmé</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each filteredMedicalActs as medicalAct}
+                <tr>
+                    <th>{medicalAct.date_prevue.toLocaleDateString('en-GB')}</th>
+                    <th>{getTime(medicalAct.date_prevue)}</th>
+                    <th>{medicalAct.id}</th>
+                    <th>{medicalAct.user_id}</th>
+                    <th>{medicalAct.confirmation_rdv ? '✅ oui' : '❌ non'}</th>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </div>
